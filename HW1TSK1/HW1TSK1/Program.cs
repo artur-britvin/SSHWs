641616d53e64f4fd11d307b1d25bb94b88b941ba
Артур Бритвин <artur.britvin@gmail.com>
2020-01-10 00:45:45 +0200

    Home Work Task 1: Done Task 2: Work in progress

---
+++ HW1TSK1/HW1TSK1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace HW1TSK1
+{
+    class Program
+    {
+        public static int GetSequence(int n)
+        {
+            string digitSequence = String.Empty;
+
+            for (int i = 9; i > 0; i--)
+            {
+                if (n >= 0 && n <= 9)
+                {
+                    digitSequence += n;
+                    return Convert.ToInt32(String.Concat(digitSequence.OrderBy(x => x)));
+                }
+                else if (i == 1 || n >= 0 && n <= 109)
+                {
+                    return -1;
+                }
+                else if (n % i == 0)
+                {
+                    n /= i;
+                    digitSequence += i;
+                    i = 9;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Please, input N number: ");
+            Console.WriteLine("Result: {0} ", GetSequence(Convert.ToInt32(Console.ReadLine())));
+        }
+    }
+}
---
+++ HW1TSK1/HW1TSK2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW1TSK2
+{
+    class Program
+    {
+        static int res = 0;
+        private static void moveBrick(ref List<int> list, int index)
+        {
+
+            list[index]--;
+            list[index + 1]++;
+            while (list[index] >= list[index + 1])
+            {
+                res++;
+                moveBrick(ref list, index + 1);
+
+
+            }
+            if (list[index] < list[index + 1])
+            {
+                list[index]++;
+                list[index + 1]--;
+            }
+            for (int j = index + 2; j < list.Count; j++)
+            {
+                list[index + 1] += list[j];
+                list[j] = 0;
+            }
+
+        }
+
+        public static int foo(int n)
+        {
+            List<int> list = new List<int>(2);
+
+            if (n <= 5 && n >= 500)
+            {
+                return -1;
+            }
+
+            list.Add(n);
+
+
+
+            list.Add(0);
+            list.Add(0); list.Add(0); list.Add(0);
+            list.Add(0); list.Add(0);
+            while (list[0] >= list[1])
+            {
+                moveBrick(ref list, 0);
+            }
+
+
+
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(foo(6));
+        }
+    }
+}
